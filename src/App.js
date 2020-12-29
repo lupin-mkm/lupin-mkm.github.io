1aefc0db9a32df14ffcf518f34e50953b62f3554
lupin-mkm <takitam@i.softbank.jp>
2020-12-29 22:37:21 +0900

    [WIP] create navbar

--- src/App.js
+++ src/App.js
@@ -3,23 +3,35 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    Button()
+  )
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+}
+
+function Button() {
+  return (
+    <div className="Button">
+      <button className="button">ボタン
+      </button>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>WORKS</li>
+          <li>FEATURE</li>
+          <li>ABOUT</li>
+          <li>SKILL</li>
+          <li>CONTACT</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
